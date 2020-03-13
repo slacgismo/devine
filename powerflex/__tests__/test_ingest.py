ed39671b7d0103e9570353339c75c8dfd681de3f
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2020-03-13 09:23:45 -0700

    add tests and tweak the ci config to run the tests after installing the dependencies

---
+++ powerflex/__tests__/test_ingest.py
@@ -0,0 +1,8 @@
+import unittest
+from ingest import *
+
+class TestDataIngest(unittest.TestCase):
+    def test_base_headers(self):
+        expected_headers = {"cache-control": "no-cache", "content-type": "application/json"}
+        result = get_request_base_headers()
+        self.assertDictEqual(result, expected_headers)
