af05f5a052b4b14a8adb8c180a270e8a66b0b425
zzx <zzx_2015@buaa.edu.cn>
2020-11-02 12:00:53 -0800

    Finish save stations data

--- app/devine/views.py
+++ app/devine/views.py
@@ -9,31 +9,34 @@ def home(request):
     return render(request, 'home.html')
 
 def db_update_from_chargepoint():
-    retvals = cp.read_from_sites()
+    retvals_all_groups = cp.read_from_sites()
     print('+'*100)
-    for retval in retvals:
-        print(retval[0]['Port'])
-        print(len(retval[0]['Port']))
-        for i in range(len(retval['Port'])): 
-            # update properties for station
-            station = db_station()
-            try:
-                station = db_station.objects.get(station_id = retval['station_id'], port_number = retval['Port'][i]['port_number'])
-            except:
-                print("new station")
-                station.station_id = retval['station_id']
-                station.port_number = retval['Port'][i]['port_number']
-            
-            station.station_load = retval['station_load']   
-            # update properties for ports
-            station.port_status = retval['Port'][i]['port_status']
-            station.shed_state = retval['Port'][i]['shed_state']
-            station.port_load = retval['Port'][i]['port_load']
-            station.allowed_load = retval['Port'][i]['allowed_load']
-            station.port_power = retval['Port'][i]['port_power']
-            #TODO: update user
-            
-            station.save()
+    for retvals_one_group in retvals_all_groups:
+        for retval_one_station in retvals_one_group:
+            print(retval_one_station)
+            # print(retval_one_station[0]['Port'])
 
+            for retval in retval_one_station['Port']:
+                # update properties for station
+                station = db_station()
+                try:
+                    station = db_station.objects.get(station_id = retval_one_station['station_id'], port_number = retval['port_number'])
+                except:
+                    print("new station")
+                    station.station_id = retval_one_station['station_id']
+                    station.port_number = retval['port_number']
+                
+                station.station_load = retval_one_station['station_load']   
+                # update properties for ports
+                station.port_status = retval['port_status']
+                station.shed_state = retval['shed_state']
+                station.port_load = retval['port_load']
+                station.allowed_load = retval['allowed_load']
+                station.port_power = retval['port_power']
+                station.port_timestamp = retval['port_timestamp']
+                #TODO: update user
+                
+                station.save()
+                print(1/0)
 
     
